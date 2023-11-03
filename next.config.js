067393e222c207cf9eb9b54bf7120107fd08717c
Rafael <velofuri@gmail.com>
2023-11-03 16:40:27 -0300

    reverce proxy 2

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/controledecompras',
-        destination: 'https://gerenciador-de-compras.vercel.app/',
+        destination: 'https://gerenciador-de-compras.vercel.app',
       },
     ]
   },
