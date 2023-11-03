6954d96d4379b9765faefe56bb8cf9f30fbe764b
Rafael <velofuri@gmail.com>
2023-11-03 16:36:19 -0300

    reverse proxy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/controledecompras',
+        destination: 'https://gerenciador-de-compras.vercel.app/',
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
